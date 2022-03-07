0cf111ebaaac1c07f36941f213ba2c54cf18c3b6
Tobias <tobidecan1@gmail.com>
2022-03-07 16:43:07 -0300

    estructura html y simulador JS

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
-// ejercicio practica ciclos
-const notas = [10 , 3 , 6 , 7 , 2]
-
-for (let i = 0 ; i < 5 ; i++){
-  if (notas[i] >= 6){
-    console.log(notas[i] + " Aprobado")
-  }
-  else{
-    console.log(notas[i])
-    let resto = 6 - notas[i]
-    console.log("Faltaron " + resto +  " puntos para aprobar")
-  }
+function AgregarAlCarrito(nombreDelProducto , precio , cantidadStock ){
+    let cantidadPedida = Number(prompt(`¿Qué cantidad desea comprar?`))
+    const total = precio * cantidadPedida
+    const validarStock = (cantidadStock >= cantidadPedida)
+        if (validarStock){
+        console.log(`Agregaste al carrito ${cantidadPedida} ${nombreDelProducto} a $${total}`)
+        } else{
+            console.log(`No hay suficiente stock de este producto`)
+        }
 }
+function MasProductos (){
+    let eleccionDeProducto = prompt(`También podria interesarle: \n 1) Swimming - Mac Miller \n 2)Blonde - Frank Ocean \n 3) El Disko - Ca7riel \n Ingrese el número del producto que desea comprar, para salir, ingrese 0`)
+    do{
+        switch (eleccionDeProducto){
+            case "1":
+                AgregarAlCarrito("Swimming - Mac Miller" , 5.499 , 5)
+                break;
+            case "2":
+                AgregarAlCarrito("Blonde - Frank Ocean" , 4.499 , 0)
+                break;
+            case "3":
+                AgregarAlCarrito("El Disko - Ca7riel" , 5.499 , 11)
+                break;
+            case "0":
+                break;
+            default:
+                alert(`El valor ingresado no es válido`)
+                eleccionDeProducto = prompt(`También podria interesarle: \n 1) Swimming - Mac Miller \n 2)Blonde - Frank Ocean \n 3) El Disko - Ca7riel \n Ingrese el número del producto que desea comprar, para salir, ingrese 0`) 
+        }
+    }while ((eleccionDeProducto < 0) || (eleccionDeProducto > 3))
+    alert(`¡Gracias por comprar en Rekord House!`)
+}
+
+//Tobías De Cancellis
