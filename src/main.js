253bd9fd5a6e26f3f1d62f8004b44c5c641c4909
Tobias <tobidecan1@gmail.com>
2022-03-16 23:15:59 -0300

    barra de filtrado y busqueda

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ const productos = [
     {id: 2 , titulo: `Swimming - Mac Miller`, genero: `HipHop` , precio: 5.499 , stock: 5 , cantidad:0 , imagen: `https://indierocks.b-cdn.net/wp-content/uploads/2018/08/Mac-Miller.png`},
     {id: 3 , titulo: `A Moon Shaped Pool - Radiohead`, genero: `Rock Alternativo` , precio: 5.499 , stock: 5 , cantidad:0 , imagen: `https://m.media-amazon.com/images/I/815bmGN5LML._SL1200_.jpg`},
     {id: 4 , titulo: `In Rainbows - Radiohead`, genero: `Rock Alternativo` , precio: 4.999 , stock: 8 , cantidad:0 , imagen: `https://canchageneral.com/wp-content/uploads/2017/10/rainbows.jpg`},
-    {id: 5 , titulo: `El Disko - Ca7riel`, genero: `HipHop` , precio: 5.499 , stock: 11 , cantidad:0 , imagen:`https://indiehoy.com/wp-content/uploads/2021/11/ca7riel.jpg`},
+    {id: 5 , titulo: `El Disko - Ca7riel`, genero: `HipHop`, precio: 5.499 , stock: 11 , cantidad:0 , imagen:`https://indiehoy.com/wp-content/uploads/2021/11/ca7riel.jpg`},
+    {id: 6 , titulo: `Circles - Mac Miller`, genero: `HipHop`, precio: 5.999 , stock: 10 , cantidad:0 , imagen:`https://www.tierraadentro.cultura.gob.mx/wp-content/uploads/2020/03/mc.jpg`},
+    {id: 7 , titulo: `The Great Escape - Volen Sentir`, genero: `Dance/Electrónica`, precio: 4.999 , stock: 3 , cantidad:0 , imagen:`https://geo-media.beatport.com/image_size/500x500/e5b013d5-9ef4-4ff8-8161-f478a622934c.jpg`},
+    {id: 8 , titulo: `Blue Train - John Coltrane`, genero: `Jazz`, precio: 4.299 , stock: 6 , cantidad:0 , imagen:`https://img.discogs.com/PCpyxXFcalQRvM5YIqdVQW4fDqo=/fit-in/596x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-401058-1362858539-5386.jpeg.jpg`},
 ]
 
 function MasProductos (){
@@ -14,13 +17,13 @@ function MasProductos (){
     do{
         switch (eleccionDeProducto){
             case "1":
-                producto3.agregarAlCarrito(producto3)
+                AgregarAlCarrito(productos[2])
                 break;
             case "2":
-                producto2.agregarAlCarrito(producto2)
+                AgregarAlCarrito(productos[1])
                 break;
             case "3":
-                producto6.agregarAlCarrito(producto6)
+                AgregarAlCarrito(productos[5])
                 break;
             case "0":
                 break;
@@ -73,6 +76,15 @@ function MostrarCardsHTML(cards){
 GenerarCards(productos)
 
 
+function buscarProducto(){
+    const productoBuscado = document.getElementById("busqueda").value.trim().toUpperCase()
+
+    const productoEncontrado = productos.filter((elemento) => {
+        return elemento.titulo.toUpperCase().match(productoBuscado)
+    })
+
+    GenerarCards(productoEncontrado)
+}
 
 
 
